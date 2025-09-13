0741efebb80087ff5f52978b68cec1b7659a7cfd
Tiago Ramon <tiagoramon.sousa12@gmail.com>
2025-09-13 11:11:11 -0300

    feat: add responsiviness on banner section about me

--- app/components/about-me/banner-section-about-me.tsx
+++ app/components/about-me/banner-section-about-me.tsx
@@ -1,64 +1,65 @@
+import ImageHuman from '@/app/assets/image-human.png'
+import Image from 'next/image';
+
 export default function BannerSectionAboutMe() {
   return (
     <section className="framer-182b2sm" data-framer-name="Banner Section">
       <div className="framer-1dw50ht" data-framer-name="Container">
-        <div className="ssr-variant hidden-xme965 hidden-p0mfc">
-          <div className="framer-hu1dfk" data-framer-name="Content">
-            <div
-              data-framer-background-image-wrapper="true"
+        <div className="framer-hu1dfk" data-framer-name="Content">
+          <div
+            data-framer-background-image-wrapper="true"
+            style={{
+              position: "absolute",
+              borderRadius: "inherit",
+              inset: 0,
+            }}
+          >
+            <Image
+              decoding="async"
+              width={3449}
+              height={1500}
+              sizes="min(100vw - 60px, 1200px)"
+              src={ImageHuman}
+              alt=""
               style={{
-                position: "absolute",
+                display: "block",
+                width: "100%",
+                height: "100%",
                 borderRadius: "inherit",
-                inset: 0,
+                objectPosition: "center bottom",
+                objectFit: "cover",
+              }}
+            />
+          </div>
+          <div className="framer-127ljvc" data-framer-name="Title Wrapper">
+            <div
+              className="framer-1rq9jw5"
+              data-framer-component-type="RichTextContainer"
+              style={{
+                outline: "none",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "flex-start",
+                flexShrink: 0,
+                transform: "none",
               }}
             >
-              <img
-                decoding="async"
-                width={3900}
-                height={1500}
-                sizes="min(100vw - 60px, 1200px)"
-                srcSet="https://framerusercontent.com/images/Q8wjWruVbYhy7d8R6Pwcx81dXo.png?scale-down-to=512 512w,https://framerusercontent.com/images/Q8wjWruVbYhy7d8R6Pwcx81dXo.png?scale-down-to=1024 1024w,https://framerusercontent.com/images/Q8wjWruVbYhy7d8R6Pwcx81dXo.png?scale-down-to=2048 2048w,https://framerusercontent.com/images/Q8wjWruVbYhy7d8R6Pwcx81dXo.png 3900w"
-                src="https://framerusercontent.com/images/Q8wjWruVbYhy7d8R6Pwcx81dXo.png"
-                alt=""
+              <h2
+                className="framer-text"
                 style={{
-                  display: "block",
-                  width: "100%",
-                  height: "100%",
-                  borderRadius: "inherit",
-                  objectPosition: "center center",
-                  objectFit: "cover",
-                }}
-              />
-            </div>
-            <div className="framer-127ljvc" data-framer-name="Title Wrapper">
-              <div
-                className="framer-1rq9jw5"
-                data-framer-component-type="RichTextContainer"
-                style={{
-                  outline: "none",
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "flex-start",
-                  flexShrink: 0,
-                  transform: "none",
-                }}
+                  "--font-selector": "R0Y7UG9wcGlucy1yZWd1bGFy",
+                  "--framer-font-family":
+                    '"Poppins", "Poppins Placeholder", sans-serif',
+                  "--framer-font-size": "38px",
+                  "--framer-letter-spacing": "-0.03em",
+                  "--framer-line-height": "48px",
+                  "--framer-text-alignment": "center",
+                  "--framer-text-color":
+                    "var(--token-b25fd8ad-c9e3-4471-b11c-08fc2ed02e4e, rgb(17, 17, 51))",
+                } as React.CSSProperties}
               >
-                <h1
-                  className="framer-text"
-                  style={{
-                    "--font-selector": "R0Y7UG9wcGlucy1yZWd1bGFy",
-                    "--framer-font-family": `"Poppins", "Poppins Placeholder", sans-serif`,
-                    "--framer-font-size": "48px",
-                    "--framer-letter-spacing": "-0.03em",
-                    "--framer-line-height": "135%",
-                    "--framer-text-alignment": "left",
-                    "--framer-text-color":
-                      "var(--token-b25fd8ad-c9e3-4471-b11c-08fc2ed02e4e, rgb(17, 17, 51))",
-                  }}
-                >
-                  A origem <br className="framer-text" />é humana
-                </h1>
-              </div>
+                A origem é humana
+              </h2>
             </div>
           </div>
         </div>
@@ -67,188 +68,4 @@ export default function BannerSectionAboutMe() {
   );
 }
 
-{
-  /* <header className="framer-16kgtac" data-framer-name="Header">
-  <div className="ssr-variant hidden-byxa9d hidden-1acmcks">
-    <div className="framer-myqk6f-container">
-      <nav
-        className="framer-ViMqZ framer-uDghj framer-6Ubzf framer-9qkq52 framer-v-11pljeq"
-        data-framer-name="Tablet Open"
-        data-hide-scrollbars="true"
-        style={{
-          backgroundColor: "var(--token-ae325ebd-12af-4f1b-9546-d54d17049faf, rgb(255, 255, 255))",
-          maxWidth: "100%",
-          width: "100%",
-          transform: "none",
-          transformOrigin: "50% 50% 0px",
-          willChange: "auto",
-        }}
-      >
-        <div
-          className="framer-9jm6cy"
-          data-framer-name="Logo and Link"
-          style={{
-            backgroundColor: "rgba(0, 0, 0, 0)",
-            borderRadius: "10px",
-            transform: "none",
-            transformOrigin: "50% 50% 0px",
-          }}
-        >
-          <div
-            className="framer-1m4qeh8"
-            data-framer-name="Name"
-            style={{
-              backgroundColor: "var(--token-ae325ebd-12af-4f1b-9546-d54d17049faf, rgb(255, 255, 255))",
-              transform: "none",
-              transformOrigin: "50% 50% 0px",
-              willChange: "auto",
-            }}
-          >
-            <a
-              className="framer-qrftsy framer-qivr5k"
-              data-framer-name="Logo and Name"
-              href="./"
-              style={{
-                borderRadius: "25px",
-                transform: "none",
-                transformOrigin: "50% 50% 0px",
-              }}
-            >
-              <div
-                className="framer-1h7zo0a"
-                data-framer-name="Logo"
-                style={{
-                  transform: "none",
-                  transformOrigin: "50% 50% 0px",
-                }}
-              >
-                <div
-                  data-framer-background-image-wrapper="true"
-                  style={{
-                    position: "absolute",
-                    borderRadius: "inherit",
-                    inset: 0,
-                  }}
-                >
-                  <img
-                    decoding="async"
-                    width={163}
-                    height={18}
-                    src="https://framerusercontent.com/images/0FyckVgb0VflkVWjHkjjnSvjjU.svg"
-                    alt="Logo"
-                    style={{
-                      display: "block",
-                      width: "100%",
-                      height: "100%",
-                      borderRadius: "inherit",
-                      objectPosition: "center center",
-                      objectFit: "contain",
-                    }}
-                  />
-                </div>
-              </div>
-            </a>
 
-            <div
-              className="framer-v4h3t3"
-              data-framer-name="Icon"
-              data-highlight="true"
-              tabIndex={0}
-              style={{
-                transform: "none",
-                transformOrigin: "50% 50% 0px",
-              }}
-            >
-              <div
-                className="framer-lsbi47"
-                data-framer-name="Bottom"
-                style={{
-                  backgroundColor:
-                    "var(--token-a3c1e9b9-e8ac-4688-91db-1f11e12741f4, rgb(22, 58, 25))",
-                  borderRadius: "41.6667% / 500%",
-                  transform: "rotate(-45deg)",
-                  transformOrigin: "50% 50% 0px",
-                  willChange: "transform",
-                }}
-              ></div>
-              <div
-                className="framer-129yrtc"
-                data-framer-name="Top"
-                style={{
-                  backgroundColor:
-                    "var(--token-a3c1e9b9-e8ac-4688-91db-1f11e12741f4, rgb(22, 58, 25))",
-                  borderRadius: "41.6667% / 500%",
-                  transform: "rotate(45deg)",
-                  transformOrigin: "50% 50% 0px",
-                  willChange: "transform",
-                }}
-              ></div>
-            </div>
-          </div>
-
-          <div
-            className="framer-rvsp47"
-            data-framer-name="Menu Wrapper"
-            style={{
-              backgroundColor: "var(--token-e4b648fd-a0ba-4e75-9577-da4e22225dcb, rgb(255, 255, 255))",
-              transform: "none",
-              transformOrigin: "50% 50% 0px",
-              willChange: "auto",
-            }}
-          >
-            <div
-              className="framer-jh61ex"
-              data-border="true"
-              data-framer-name="Links"
-              style={{
-                "--border-bottom-width": "0px",
-                "--border-color": "rgba(255, 255, 255, 0.1)",
-                "--border-left-width": "0px",
-                "--border-right-width": "0px",
-                "--border-style": "solid",
-                "--border-top-width": "1px",
-                backgroundColor: "rgba(0, 0, 0, 0)",
-                borderRadius: "25px",
-                transform: "none",
-                transformOrigin: "50% 50% 0px",
-                willChange: "auto",
-              } as React.CSSProperties}
-            >
-              {["palestras e workshops", "sobre mim", "contato", "HOME"].map((label, idx) => (
-                <div
-                  key={idx}
-                  className="framer-1sbc5io"
-                  data-framer-component-type="RichTextContainer"
-                  style={{
-                    outline: "none",
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "flex-start",
-                    flexShrink: 0,
-                    "--framer-paragraph-spacing": "0px",
-                    transform: "none",
-                    transformOrigin: "50% 50% 0px",
-                  } as React.CSSProperties}
-                >
-                  <p
-                    className="framer-text framer-styles-preset-755d1"
-                    style={{
-                      "--framer-text-color":
-                        "var(--token-3696beb0-9bcd-4868-8f06-aad504012b0e, rgb(0, 0, 0))",
-                    } as React.CSSProperties}
-                  >
-                    <a className="framer-text framer-styles-preset-9gqfxb" href="./">
-                      {label}
-                    </a>
-                  </p>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      </nav>
-    </div>
-  </div>
-</header>
- */
-}
