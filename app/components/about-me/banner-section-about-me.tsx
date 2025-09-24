91ec7bcb501823e42a6658c4329374af09c2ce16
Tiago Ramon <tiagoramon.sousa12@gmail.com>
2025-09-24 13:30:53 -0300

    feat: adjusting banner section about me image human

--- app/components/about-me/banner-section-about-me.tsx
+++ app/components/about-me/banner-section-about-me.tsx
@@ -18,8 +18,6 @@ export default function BannerSectionAboutMe() {
               decoding="async"
               width={3449}
               height={1500}
-              sizes="min(1024vw - 60px, 1200px)"
-              unoptimized
               src={ImageHuman}
               alt=""
               style={{
